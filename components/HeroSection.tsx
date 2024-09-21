b1cf336189505b6348d226bddd3c307fd24d66f8
anthonykimani <kimaniantoni@gmail.com>
2024-09-21 13:55:39 +0300

    feat: adds framer motion

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,10 +1,11 @@
 import { Universe } from "@/constants/svg";
 import Image from "next/image";
 import React from "react";
+import * as motion from "framer-motion/client";
 
 const HeroSection = () => {
   return (
-    <main className="flex flex-col justify-around sm:justify-start items-center text-center bg-hero-bg bg-no-repeat bg-center bg-cover overflow-hidden px-3 md:px-24">
+    <main className="flex flex-col justify-around sm:justify-start items-center text-center overflow-hidden px-3 md:px-24">
       <h1 className="bg-gradient-to-b from-[#ffffff] to-[#ffffff70] bg-clip-text text-transparent text-4xl md:text-[50px] lg:text-[60px] font-clashDisplay font-semibold flex flex-col justify-around leading-[40px] lg:leading-[70px] mt-[100px]">
         Platform for tech builders in Web3, Blockchain, Metaverse, and AI
       </h1>
@@ -17,7 +18,16 @@ const HeroSection = () => {
       <button className="py-2 px-6  rounded-full  bg-gradient-to-br from-[#8BC2FF] to-[#2D55FF] font-semibold hover:bg-white hover:cursor-pointer">
         Join Umojaverse
       </button>
-      <Image src={Universe} alt="" className=""/>
+      <motion.div
+      animate={{ rotate: 360 }}
+      transition={{
+        duration: 60,
+        repeat: Infinity,
+        ease: "linear"
+      }}
+    >
+        <Image src={Universe} alt="" className="" />
+      </motion.div>
     </main>
   );
 };
